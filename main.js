a44b6506d8f8ecd04302d9d013fe21015aa268b8
5en5e <dmitriy11993@yandex.ru>
2026-04-05 14:58:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,413 @@
+function ll(x){
+  console.log(x);
+}
+
+let allAlbums = [
+  {"id":"moments-of-life-258","file":"https://promodj.com/prelisten/7497450/Wind_Of_Buri_Moments_Of_Life_258_Guitar_Chill_Mix.mp3?hq=1", "title":"Wind Of Buri - Moments Of Life 258 (Guitar - Chill Mix)","tracks":[{"performer":"Nicholas Gunn","starttime":"0:00:00","title":"Chasing The Light"},{"performer":"One Arc Degree","starttime":"4:07:00","title":"The Ocean Palace (Part 1)"},{"performer":"Sweet Velvet","starttime":"9:09:00","title":"Brightness"},{"performer":"Cullera","starttime":"13:24:00","title":"Sun & Sea (Balearic Breeze) (Chillout Mix)"},{"performer":"Tenisha","starttime":"17:37:00","title":"Across The Border (Chillout Mix)"},{"performer":"Orjan Nilsen","starttime":"20:47:00","title":"La Guitarra (M.T.C. Remix)"},{"performer":"York & Rank 1 Feat. Lola","starttime":"24:36:00","title":"This World Is Amazing"},{"performer":"Rayan Myers Feat. Nesa","starttime":"28:10:00","title":"Limerence"},{"performer":"The Thrillseekers Pres. Hydra","starttime":"34:29:00","title":"Where Oceans Meet (Part III) (Edit)"},{"performer":"Nicholas Gunn","starttime":"38:22:00","title":"Be Tulum"},{"performer":"Airstream","starttime":"41:54:00","title":"Sweet Life (Dream Wave Mix)"},{"performer":"Electric Entourage","starttime":"46:42:00","title":"You Look So High"},{"performer":"Matan Arkin","starttime":"51:04:00","title":"Gipsy"},{"performer":"Blue Lunar Monkey","starttime":"55:52:00","title":"Union"},{"performer":"Narcotic Chill","starttime":"60:49:00","title":"On The Brink"},{"performer":"Prior Matt & Toff Alex","starttime":"64:14:00","title":"Space Bubble"},{"performer":"Jason Van Wyk","starttime":"66:20:00","title":"As You Sleep"},{"performer":"Adam Nickey","starttime":"69:33:00","title":"Perfect Destiny (Solarsoul Chillout Remix)"},{"performer":"Changer","starttime":"75:39:00","title":"Sunrise Breeze (Chillout Mix)"}]},
+  {"id":"moments-of-life-259","file":"https://promodj.com/prelisten/7497753/Wind_Of_Buri_Moments_Of_Life_259_Acoustic_Mix.mp3?hq=1", "title":"Wind Of Buri - Moments Of Life 259 (Acoustic Mix)", "tracks":[{"performer":"London Grammar","starttime":"0:00:00","title":"Truth Is A Beautiful Thing"},{"performer":"Markus Schulz Feat. Emma Hewitt","starttime":"4:43:00","title":"Safe From Harm (Acoustic Version)"},{"performer":"Christina Novelli","starttime":"7:28:00","title":"Be Without You"},{"performer":"Tigerforest & Tim Gelo Feat. Vancouver","starttime":"9:40:00","title":"Keep On Running (Der Waldlaufer Remix)"},{"performer":"Tritonal & SJ Feat. Emma Gatsby","starttime":"14:19:00","title":"Hung Up (Acoustic Mix)"},{"performer":"Thomas Lemmer Feat. Naemi Joy","starttime":"18:00:00","title":"White Room (Intimate Piano Version)"},{"performer":"Nicholas Gunn Feat. Alina Renae","starttime":"21:49:00","title":"And I"},{"performer":"Narrow Skies","starttime":"24:53:00","title":"I Wonder"},{"performer":"C-Systems Feat. Hanna Finsen","starttime":"27:16:00","title":"Feeling Fine (Acoustic Mix)"},{"performer":"Above & Beyond Feat. Zoe Johnston","starttime":"31:27:00","title":"Always"},{"performer":"Blank & Jones Feat. Maria Marcus","starttime":"35:28:00","title":"Snow Storm"},{"performer":"All India Radio","starttime":"38:56:00","title":"Morning Drops (Acoustic Mix)"},{"performer":"Markus Schulz Feat. Alina Eremia","starttime":"42:29:00","title":"You Light Up The Night (Acoustic Version)"},{"performer":"The Haiku Project Feat. Sara Grabow","starttime":"46:56:00","title":"Creation"},{"performer":"Susana","starttime":"53:49:00","title":"A Million Memories (Acoustic Rework)"},{"performer":"Christina Novelli","starttime":"57:03:00","title":"Beautiful Life"},{"performer":"Ameera","starttime":"59:36:00","title":"Home"},{"performer":"Alan Morris & Enzo Feat. Jess Morgan","starttime":"63:03:00","title":"Tapestry Of Us (Piano Acoustic Mix)"},{"performer":"Steve Allen & Paul Skelton & Deirdre McLaughlin","starttime":"65:58:00","title":"Fairytales (Chill Out Remix)"},{"performer":"CHVRCHES","starttime":"68:09:00","title":"Afterglow"},{"performer":"Whirl & Mayer","starttime":"70:54:00","title":"Heaven Above (Sunrise Mix)"}]},
+  {"id":"moments-of-life-265","file":"https://promodj.com/prelisten/7666492/Wind_Of_Buri_Moments_Of_Life_265_Guitar_Chill_Mix.mp3?hq=1", "title":"Wind Of Buri - Moments Of Life 265 (Guitar - Chill Mix)", "tracks":[{"performer":"York With Nathan Red & Jorg Stenzel","starttime":"0:00:00","title":"Burning Flame"},{"performer":"Insan","starttime":"6:23:00","title":"Sail In The Vacuum"},{"performer":"G Club Pres. Banda Sonora","starttime":"10:01:00","title":"Guitarra G (Afterlife Mix)"},{"performer":"Physical Dreams","starttime":"15:00:00","title":"El Espanol"},{"performer":"Michael E","starttime":"23:08:00","title":"Downtown"},{"performer":"Tony Sit","starttime":"25:45:00","title":"Space Guitar"},{"performer":"Rocket Empire","starttime":"29:49:00","title":"Beijing"},{"performer":"Gary B & Ken Fan","starttime":"32:48:00","title":"Dulce Dub"},{"performer":"Chris Wonderful","starttime":"36:10:00","title":"Summer Sun"},{"performer":"Nicholas Gunn","starttime":"41:24:00","title":"Ibiza Sunset"},{"performer":"Suduaya","starttime":"44:49:00","title":"Ajna"},{"performer":"Bardalimov","starttime":"52:29:00","title":"Running A Wrong Way"},{"performer":"Sine","starttime":"57:09:00","title":"Glittering Water"},{"performer":"Doug Blair","starttime":"62:33:00","title":"The End"},{"performer":"Ivan Latyshev & Trophieme","starttime":"67:11:00","title":"Brothers In The Wind"},{"performer":"York","starttime":"72:31:00","title":"Be A Giant"}]},
+  {"id":"moments-of-life-268","file":"https://promodj.com/prelisten/7823162/Wind_Of_Buri_Moments_Of_Life_268_Vocal_Chill_Mix.mp3?hq=1", "title":"Wind Of Buri - Moments Of Life 268 (Vocal - Chill Mix)","tracks":[{"performer":"Solid Stone Feat. Arielle Maren","starttime":"0:00:00","title":"Hurricane (Chillout Mix)"},{"performer":"The Thrillseekers Feat. Stine Grove","starttime":"8:40:00","title":"Anywhere With You"},{"performer":"Gushi & Raffunk","starttime":"11:41:00","title":"Travel (Alexandra Hampton Remix)"},{"performer":"Jane Maximova","starttime":"14:57:00","title":"Novel"},{"performer":"Yaima","starttime":"18:37:00","title":"Citadella (Revisioned)"},{"performer":"Melounge","starttime":"22:30:00","title":"Too Much Sugar (When Bedroom Love Takes Over Mix)"},{"performer":"Denox","starttime":"26:43:00","title":"Year Of The Monkey"},{"performer":"Similar Taste","starttime":"31:43:00","title":"Let It All Out"},{"performer":"Schiller Feat. Tricia McTeague","starttime":"36:10:00","title":"Miracle"},{"performer":"The Urban City Myth Club","starttime":"40:48:00","title":"I Feel It"},{"performer":"Vargo","starttime":"44:04:00","title":"Talking One Language (25Th Anniversary Mix)"},{"performer":"Symphony Of Science","starttime":"48:26:00","title":"The Cosmic Dance (Mindwalk Remix)"},{"performer":"Paul Seling","starttime":"51:10:00","title":"Sad Eyes"},{"performer":"Helicopter Girl","starttime":"54:37:00","title":"Umbrellas In The Rain"},{"performer":"The Sound Of Love","starttime":"58:25:00","title":"Your Mind"},{"performer":"Since Now & Xeon","starttime":"61:24:00","title":"Fall Deep"},{"performer":"Marcon & Val Helix","starttime":"66:31:00","title":"Chance For All (Division 4 Ibiza Chill Remix)"},{"performer":"Rayan Myers","starttime":"70:22:00","title":"Borrowing"},{"performer":"Derrick","starttime":"73:21:00","title":"Just To Be (Spring Version)"}]}
+];
+
+allAlbums.sort((a, b) => b.title.localeCompare(a.title));
+
+
+
+let currentAlbum = undefined;
+let controls = {};
+let currentTrack = undefined;
+let currentState = "none";
+const audio = new Audio();
+
+
+function foo(e){
+  console.log(e.type);
+}
+
+//audio.addEventListener("abort", foo);
+//audio.addEventListener("canplay", foo);
+//audio.addEventListener("canplaythrough", foo);
+//audio.addEventListener("durationchange", foo);
+//audio.addEventListener("emptied", foo);
+//audio.addEventListener("encrypted", foo);
+//audio.addEventListener("ended", foo);
+//audio.addEventListener("error", foo);
+//audio.addEventListener("loadeddata", foo);
+//audio.addEventListener("loadedmetadata", foo);
+//audio.addEventListener("loadstart", foo);
+//audio.addEventListener("pause", foo);
+//audio.addEventListener("play", foo);
+//audio.addEventListener("playing", foo);
+//audio.addEventListener("progress", foo);
+//audio.addEventListener("ratechange", foo);
+//audio.addEventListener("seeked", foo);
+//audio.addEventListener("seeking", foo);
+//audio.addEventListener("stalled", foo);
+//audio.addEventListener("suspend", foo);
+//audio.addEventListener("timeupdate", foo);
+//audio.addEventListener("volumechange", foo);
+//audio.addEventListener("waiting", foo);
+//audio.addEventListener("waitingforkey", foo);
+
+
+function throttle(fn, delay) {
+  let lastCall = 0;
+  let timeout = null;
+
+  return function (...args) {
+    const now = Date.now();
+
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      fn.apply(this, args);
+    } else if (!timeout) {
+      timeout = setTimeout(() => {
+        lastCall = Date.now();
+        timeout = null;
+        fn.apply(this, args);
+      }, delay - (now - lastCall));
+    }
+  };
+}
+
+updateProgressBarThr=throttle(updateProgressBar, 500);
+
+audio.addEventListener("timeupdate", (event) => {
+  updateTrack();
+});
+
+
+function updateTrack(){
+  updateProgressBar(formatCurrentTime(audio.currentTime), audio.currentTime/audio.duration);
+  let playingTrack=getTrackByTime(audio.currentTime);
+  if (currentTrack!=playingTrack){
+    if (document.querySelector(".misc .loop").getAttribute("state")=="on"){
+      playCurrentTrack();
+    } else {
+      currentTrack=playingTrack;
+      undecorateOldTrack();
+      decorateCurrentTrack();
+    }
+  }
+}
+
+function updateProgressBar(time,value){
+    //console.log("currentTime: ", formatCurrentTime(audio.currentTime));
+  document.querySelector(".audio-progress .progress-value").textContent=" "+time;
+  document.querySelector(".audio-progress .progress-value").style.width=`${value*100}%`;
+
+}
+
+
+document.querySelector(".misc .loop").addEventListener("click", (e)=>{ 
+  e.target.getAttribute("state")=="on"?e.target.setAttribute("state","off"):e.target.setAttribute("state","on");
+});
+
+function getTrackByTime(time){
+  let tracks=document.querySelectorAll(".track ");
+  for (let x of tracks){
+    let start = startTimeInSeconds(x);
+    let end = x.nextSibling?startTimeInSeconds(x.nextSibling):audio.duration;
+    if ((time>=start && time<end)){
+      return x;
+    } else if (end==audio.duration){
+        undecorateOldTrack();
+        currentState = "none";
+        toggleIcons("play");
+        return x;
+    }
+  }
+}
+
+function formatCurrentTime(currentTime){
+  const hours = parseInt(currentTime / 3600);
+  const hoursStr = hours<10?"0"+hours:`${hours}`;
+  const minutes = parseInt((currentTime / 60)%60);
+  const minutesStr = minutes<10?"0"+minutes:`${minutes}`;
+  const seconds = parseInt(currentTime % 60);
+  const secondsStr = seconds<10?"0"+seconds:`${seconds}`;
+  return `${hoursStr}:${minutesStr}:${secondsStr}`;
+}
+
+
+function populateAlbumList(){
+  const parent = document.getElementsByClassName('left-container')[0];
+
+  for (item of allAlbums){
+    const title = item.title;
+
+    const album = document.createElement('div');
+    album.classList.add('album');
+    const id = item.id;
+    album.setAttribute("atitle",`${id}`);
+
+    const cover = document.createElement('div');
+    cover.classList.add('cover');
+
+    const name = document.createElement('div');
+    name.classList.add('name');
+    name.textContent=`${title}`;
+
+    album.appendChild(cover);
+    album.appendChild(name);
+    parent.appendChild(album);
+  }
+}
+
+
+function setCurrentAlbum(id){
+  currentTrack = undefined;
+  currentState = "none";
+  toggleIcons("play");
+
+  updateProgressBar("00:00:00", 0);
+
+  currentAlbum = allAlbums.find(item => item.id === id);
+  
+  audio.pause();
+  audio.src = currentAlbum.file;
+  audio.load();
+
+  const title = currentAlbum.title;
+
+  const album = document.querySelector('.right-container .album');
+  const name = document.querySelector('.right-container .album .name');
+  album.setAttribute("atitle",`${id}`);
+  name.textContent=`${title}`;
+  
+  const tracklist = document.getElementsByClassName('tracklist')[0];
+  tracklist.innerHTML = '';
+  for (trackInfo of currentAlbum.tracks){
+    const track = document.createElement('li');
+    track.classList.add('track');
+    track.setAttribute("starttime",`${trackInfo.starttime}`);
+    track.setAttribute('active', "false");
+    currentTrack ??= track;
+
+    const pp = document.createElement('span');
+    pp.classList.add('play-pause');
+    
+
+    const trackTitle = document.createElement('span');
+    const trackPerformer = document.createElement('span');
+    trackTitle.classList.add('track-title');
+    trackPerformer.classList.add('track-performer');
+    trackTitle.textContent = `${trackInfo.title}`
+    trackPerformer.textContent = ` - ${trackInfo.performer}`
+    
+    const duration = document.createElement('span');
+    duration.classList.add('duration');
+
+    track.appendChild(pp);
+    track.appendChild(trackTitle);
+    track.appendChild(trackPerformer);
+    track.appendChild(duration);    
+    
+    tracklist.appendChild(track);
+  }
+}
+
+
+
+function playOrPause(){
+  if (currentState == "none"){
+      currentState = "playing";
+      toggleIcons("pause");
+      currentTrack = document.querySelector('.track:first-child');
+      playCurrentTrack();
+  } else if (currentState=="paused"){
+      currentState = "playing";
+      toggleIcons("pause");
+      audio.play().catch((e)=>{console.log(e)});;
+  } else if (currentState=="playing"){
+      currentState = "paused";
+      toggleIcons("play");
+      audio.pause();
+  } else {
+    throw "currentState==???";
+  }
+}
+
+function toggleIcons(icon){
+  if (icon=="play"){
+    currentTrack?.querySelector(".play-pause").setAttribute("icon", "play");
+    document.querySelector(".order .play-pause").setAttribute("icon", "play");
+    document.querySelector(".wrapper .play-pause").setAttribute("icon", "play");
+  } else if (icon=="pause"){
+    currentTrack?.querySelector(".play-pause").setAttribute("icon", "pause");
+    document.querySelector(".order .play-pause").setAttribute("icon", "pause");
+    document.querySelector(".wrapper .play-pause").setAttribute("icon", "pause");
+  } else {
+    throw "icon==???";
+  }
+}
+
+
+function playCurrentTrack(){
+  audio.pause();
+  undecorateOldTrack();
+  decorateCurrentTrack();
+  const seconds = startTimeInSeconds(currentTrack);
+  audio.currentTime=seconds;
+  currentState = "playing";
+  audio.play().catch((e)=>{console.log(e)});;
+}
+
+function undecorateOldTrack(){
+  oldTrack = document.querySelector('.track[active="true"]');
+  oldTrack?.querySelector(".play-pause").setAttribute("icon", "play");
+  oldTrack?.setAttribute("active","false");
+}
+function decorateCurrentTrack(){
+  toggleIcons("pause");
+  currentTrack.setAttribute("active","true");
+}
+
+document.querySelector(".order .prev-wrapper").addEventListener("click", (e)=>{ 
+  let newTrack = undefined;
+  if (currentState == "none"){
+    playOrPause();
+    return;
+  }
+  if (currentTrack == document.querySelector('.track:first-child')){
+    newTrack = document.querySelector('.track:last-child');
+  } else {
+    newTrack = currentTrack.previousElementSibling; 
+  }
+  currentTrack = newTrack;
+  playCurrentTrack();
+})
+
+
+
+document.querySelector(".order .play-pause-wrapper").addEventListener("click", (e)=>{ 
+  playOrPause();
+});
+
+document.querySelector(".order .next-wrapper").addEventListener("click", (e)=>{ 
+  let newTrack = undefined;
+  if (currentState == "none"){
+    playOrPause();
+    return;
+  }
+  if (currentTrack == document.querySelector('.track:last-child')){
+    newTrack = document.querySelector('.track:first-child');
+  } else {
+    newTrack = document.querySelector('.track[active="true"] + .track'); 
+  }
+  currentTrack = newTrack;
+  playCurrentTrack();
+})
+
+
+populateAlbumList();
+
+setCurrentAlbum(allAlbums[1].id);
+
+document.querySelector(".left-container").addEventListener("click", (e)=>{ 
+  const album = e.target.closest('.album');
+  if (!album){return;}
+  const title = album.getAttribute("atitle");
+  
+  if (currentAlbum.id!=title) {
+    setCurrentAlbum(title);
+  }
+})
+
+
+function startTimeInSeconds(track){
+  const str = track.getAttribute("starttime");
+  const parts = str.split(":");
+  const minutes = parseInt(parts[0], 10);
+  const seconds = parseInt(parts[1], 10);
+  return minutes * 60 + seconds;
+}
+
+document.querySelector(".tracklist").addEventListener("click", (e) => {
+  const selectedTrack = e.target.closest('.track');
+
+  if (!selectedTrack) return;
+  if (selectedTrack == currentTrack){
+    playOrPause();
+  } else{
+    currentTrack = selectedTrack;
+    currentState="playing";
+    playCurrentTrack();
+  }
+});
+
+document.querySelector(".wrapper").addEventListener("click", (e) => {
+  playOrPause();
+});
+
+
+document.querySelector(".controls .sound .mute").addEventListener("click", (e) => {
+  const vol = document.querySelector('.controls .sound .volume [type="range"]');
+  if (e.target.getAttribute("muted")=="false"){
+    audio.muted=true; 
+    controls.volume=vol.value;
+    vol.value=0;
+    e.target.setAttribute("muted", "true");   
+  }
+  else{
+    e.target.setAttribute("muted", "false"); 
+    vol.value=controls.volume;
+    audio.muted=false;
+  }
+  e.stopPropagation();
+});
+
+
+document.querySelector('.controls .sound .volume [type="range"]').addEventListener("input", (e) => {
+  audio.volume=e.target.value/100;
+  controls.volume=e.target.value+"";
+  e.stopPropagation();
+});
+
+
+
+const divider = document.querySelector('.divider');
+const leftPane = document.querySelector('.left');
+const container = document.querySelector('.split-container');
+
+let isDragging = false;
+
+divider.addEventListener('mousedown', () => {
+  document.body.classList.add('resizing');
+  isDragging = true;
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+
+  const containerRect = container.getBoundingClientRect();
+  let newLeftWidth = e.clientX - containerRect.left;
+
+  // минимальная и максимальная ширина
+  const minWidth = 50; 
+  const maxWidth = containerRect.width - 50;
+  newLeftWidth = Math.max(minWidth, Math.min(maxWidth, newLeftWidth));
+
+  leftPane.style.flexBasis = newLeftWidth + 'px';
+});
+
+document.addEventListener('mouseup', () => {
+  document.body.classList.remove('resizing');
+  isDragging = false;
+});
+
+
+
+
+document.querySelector(".audio-progress").addEventListener("click", (e) => {
+  let prop = e.offsetX / e.currentTarget.getBoundingClientRect().width;
+  audio.pause();
+
+  ct = Math.floor(prop*audio.duration);
+  audio.currentTime=ct;
+  currentTrack=getTrackByTime(audio.currentTime);
+  undecorateOldTrack();
+  decorateCurrentTrack();
+  audio.play().catch((e)=>{console.log(e)});
+  
+});
